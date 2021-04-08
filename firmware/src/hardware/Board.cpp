89b0eb333fbb30c6634427e8ce32787d7afbac11
Apache <tj.thomas.jeantet@gmail.com>
2021-04-08 22:56:01 +0200

    [CAN] Module configuration

--- firmware/src/hardware/Board.cpp
+++ firmware/src/hardware/Board.cpp
@@ -1,4 +1,20 @@
 #include "Board.hpp"
 #include "board.h"
+#include "ch.hpp"
+#include "hal.h"
+#include "BuildConf.hpp"
+#include "Logging.hpp"
 
-void Board::init() {}
+CANConfig canConfig = {
+        .btr = 0x00050007,
+//    .mcr = 0,
+};
+
+
+
+void Board::init() {
+    palSetLineMode(CAN_TX_PIN, CAN_TX_PIN_MODE);
+    palSetLineMode(CAN_RX_PIN, CAN_RX_PIN_MODE);
+    canStart(&CAN_DRIVER, &canConfig);
+
+}
--- firmware/src/hardware/param/BuildConf.hpp
+++ firmware/src/hardware/param/BuildConf.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
 #include "Board.hpp"
+#include "board.h"
 
 #define LOGGING_DRIVER SD2
 #define SHELL_DRIVER LOGGING_DRIVER
 
-#define LED_LINE LINE_LED
+#define LED_LINE LINE_LED_GREEN
+
+#define CAN_DRIVER CAND1
+
+#define CAN_TX_PIN      LINE_ARD_D2
+#define CAN_TX_PIN_MODE PAL_MODE_ALTERNATE(9)
+#define CAN_RX_PIN      LINE_ARD_D10
+#define CAN_RX_PIN_MODE PAL_MODE_ALTERNATE(9)
+
